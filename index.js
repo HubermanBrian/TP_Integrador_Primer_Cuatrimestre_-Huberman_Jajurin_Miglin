e58d8845517adaaf70a2db9a86cf56402db99fd7
Brian Huberman <Brianghuberman@gmail.com>
2025-08-01 14:31:11 -0300

    no engancha el front con el back

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const eventsRouter = require('./routes/events');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/events', eventsRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
